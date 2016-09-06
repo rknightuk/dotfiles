0b2e5d1c560ac60017e4fce166756de318d1f240
Wade Urry <wade@iwader.co.uk>
2016-09-06 19:09:34 +0100

    Setup gpg agent

---
+++ gpg/gpg.zsh
@@ -0,0 +1,6 @@
+if test -f ~/.gnupg/.gpg-agent-info -a -n "$(pgrep gpg-agent)"; then
+  source ~/.gnupg/.gpg-agent-info
+  export GPG_AGENT_INFO
+else
+  eval $(gpg-agent --daemon --write-env-file ~/.gnupg/.gpg-agent-info)
+fi
