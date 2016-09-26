b165b185041fcbaf914364d96efc47c14e6c2cfd
Wade Urry <wade@iwader.co.uk>
2016-09-26 15:08:04 +0100

    Remove docker functions for now as they're interfering

--- docker/docker.zsh
+++ docker/docker.zsh
@@ -4,12 +4,3 @@ alias dcb="docker-compose build"
 alias dcd="docker-compose down"
 alias dcs="docker-compose stop"
 alias dce="docker-compose exec"
-
-# Delete all containers
-alias drm="docker rm $(docker ps -a -q)"
-
-# Delete all images
-alias dri="docker rmi $(docker ps -a -q)"
-
-# Bash
-alias dbash="docker exec -it $(docker ps -aqf "name=$1") bash"
