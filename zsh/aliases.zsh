6ef5516d171df35e73701bc7d3e2d1a17534cf82
Wade Urry <wade@iwader.co.uk>
2016-09-26 20:45:02 +0100

    Add phpspec alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,9 +1,16 @@
 alias reload!='. ~/.zshrc'
 eval "$(thefuck --alias)"
 alias cl='clear'
+
+# Laravel
 alias hs="homestead"
 alias a="php artisan"
+
+# PHP
 alias phpunit="./vendor/bin/phpunit"
+alias phpspec="./vendor/bin/phpspec"
+
+# JS
 alias gulp="./node_modules/gulp/bin/gulp.js"
 alias karma="./node_modules/karma/bin/karma"
 alias webpack="./node_modules/webpack/bin/webpack.js"
