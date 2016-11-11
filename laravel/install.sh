636afac6b59482f2fb691aac24a643ba1094d837
Wade Urry <wade@iwader.co.uk>
2016-11-11 19:16:32 +0000

    Install laravel/spark-installer with composer

--- laravel/install.sh
+++ laravel/install.sh
@@ -5,6 +5,7 @@ COMPOSER_ARGS="--no-interaction --quiet"
 
 composer global require "laravel/installer" $COMPOSER_ARGS
 composer global require "laravel/lumen-installer":"dev-master" $COMPOSER_ARGS
+composer global require "laravel/spark-installer" $COMPOSER_ARGS
 
 if ! vagrant box list | grep --quiet "laravel\/homestead"
 then
