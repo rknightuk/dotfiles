608848164f7825e8b59d39629270e648d9d5f694
Wade Urry <wade@iwader.co.uk>
2017-02-23 11:39:54 +0000

    Remove symbol from prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -34,6 +34,7 @@ fi
 
 CURRENT_BG='NONE'
 SEGMENT_SEPARATOR=''
+DEFAULT_USER='wade'
 
 ## huh dont need this
 collapse_pwd() {
@@ -78,7 +79,7 @@ prompt_context() {
   local user=`whoami`
 
   if [[ "$user" != "$DEFAULT_USER" || -n "$SSH_CLIENT" ]]; then
-    prompt_segment black default "%(!.%{%F{yellow}%}.)✝"
+    prompt_segment black default "%(!.%{%F{yellow}%}.)"
   fi
 }
 
