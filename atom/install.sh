f7bac9c8641eb0e57201542d5a6c8ab8e466e3b1
Wade Urry <wade@iwader.co.uk>
2017-03-22 14:07:29 +0000

    Add atom packages

---
+++ atom/install.sh
@@ -0,0 +1,16 @@
+apm install editorconfig \
+            file-icons \
+            hyperclick \
+            intentions \
+            project-manager \
+            recent-files-fuzzy-finder \
+            zentabs \
+            php-composer-completion \
+            php-integrator-base \
+            php-integrator-annotations \
+            php-integrator-autocomplete-plus \
+            php-integrator-call-tips \
+            php-integrator-linter \
+            php-integrator-navigation \
+            php-integrator-refactoring \
+            php-integrator-tooltips
