2219bc2d84051b41e76593383548310c3ffab7e0
Wade Urry <wade@iwader.co.uk>
2017-04-14 10:41:02 +0100

    Only load nvm is $NVM_BIN isn't already set

--- nvm/nvm.zsh
+++ nvm/nvm.zsh
@@ -1,2 +1,6 @@
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+# Only load NVM if it hasn't already. This causes quite a lot of delay in oh-my-zsh
+if [ -z "$NVM_BIN" ];
+then
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+fi
