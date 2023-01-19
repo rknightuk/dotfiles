8bd2588453309ef1a52435212379ff5145d7d61b
Robb Knight <desk@rknight.me>
2023-01-19 11:57:53 +0000

    Add alias

--- osx/osx.zsh
+++ osx/osx.zsh
@@ -1,3 +1,12 @@
 export LIBRARY="$HOME/Library"
 
 alias flushdns="sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder;"
+alias st="networkQuality -v"
+
+function site() {
+    if [ $# -eq 0 ]; then
+        cd ~/Sites/personal/$(ls -1 "~/Sites/personal" | pick)
+    else
+        cd ~/Sites/personal/"$@"
+    fi
+}
