7a54d0b361778adcf48ac68b4975047f90354a84
Robb Knight <desk@rknight.me>
2023-06-19 23:05:58 +0100

    sql stop/start

--- laravel/aliases.zsh
+++ laravel/aliases.zsh
@@ -8,3 +8,6 @@ function laralog() {
 		tail -f $base | grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn} --line-buffered "$search"
 	fi
 }
+
+alias sqlstart='brew services start mysql'
+alias sqlstop='brew services stop mysql'
